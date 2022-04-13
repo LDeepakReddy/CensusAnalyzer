ff4ecfe7099371d95c804f6b60b973be97a534a7
LDeepakReddy <l.deepakreddy.com>
2022-04-13 15:49:20 +0530

    ADDED:Feature to Load Data From CSV File

---
+++ src/test/java/com/blz/indiacensusanalyzer/CensusAnalyzerTest.java
@@ -0,0 +1,16 @@
+package com.blz.indiacensusanalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CensusAnalyzerTest {
+    private static final String INDIAN_CENSUS_CSV_FILE_PATH="C:\\Users\\DEEPAK REDDY\\Desktop\\Java_Bridgelabz\\IndiaCensus_Analyzer\\src\\main\\resources\\IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
+
+        CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
+        int numOfRecords = censusAnalyzer.loadIndiaCensusData(INDIAN_CENSUS_CSV_FILE_PATH);
+        Assert.assertEquals(29,numOfRecords);
+    }
+}
